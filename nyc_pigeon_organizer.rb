0456555c9ef14c036975fdcf6b4fbcf8b000f0ae
github username <github email address>
2017-04-21 13:10:53 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  birds = {}
+  data.each do |description_keys, descriptions|
+    descriptions.each do |keys, names|
+      names.each do |name|
+        birds[name] ||= {}
+        birds[name][description_keys] ||= []
+        birds[name][description_keys] << keys.to_s
+      end
+    end
+  end
+  birds
 end
